a083276a7bddb4722727f4e9ed0db67626ec4dd8
Jean-Philippe Zolesio <zolesio@adobe.com>
2023-01-25 12:01:12 -0800

    Fix CI/CD issues

--- add_esm_import_extensions.mjs
+++ add_esm_import_extensions.mjs
@@ -3,96 +3,103 @@ import * as path from 'path';
 
 // https://gist.github.com/lovasoa/8691344
 async function* walk(dir) {
-	for await (const d of await fs.promises.opendir(dir)) {
-		const entry = path.join(dir, d.name);
-		if (d.isDirectory()) {
-			yield* walk(entry);
-		} else if (d.isFile()) {
-			yield entry;
-		}
-	}
+  for await (const d of await fs.promises.opendir(dir)) {
+    const entry = path.join(dir, d.name);
+    if (d.isDirectory()) {
+      yield* walk(entry);
+    } else if (d.isFile()) {
+      yield entry;
+    }
+  }
 }
 
 function resolveImportPath(sourceFile, importPath, options) {
-	const sourceFileAbs                        = path.resolve(process.cwd(), sourceFile);
-	const root                                 = path.dirname(sourceFileAbs);
-	const {moduleFilter = defaultModuleFilter} = options;
+  const sourceFileAbs = path.resolve(process.cwd(), sourceFile);
+  const root = path.dirname(sourceFileAbs);
+  const {moduleFilter = defaultModuleFilter} = options;
 
-	if (moduleFilter(importPath)) {
-		const importPathAbs = path.resolve(root, importPath);
-		let possiblePath    = [
-			path.resolve(importPathAbs, './index.ts'),
-			path.resolve(importPathAbs, './index.js'),
-			importPathAbs + '.ts',
-			importPathAbs + '.js',
-		];
+  if (moduleFilter(importPath)) {
+    const importPathAbs = path.resolve(root, importPath);
+    const possiblePath = [
+      path.resolve(importPathAbs, './index.ts'),
+      path.resolve(importPathAbs, './index.js'),
+      importPathAbs + '.ts',
+      importPathAbs + '.js',
+    ];
 
-		if (possiblePath.length) {
-			for (let i = 0; i < possiblePath.length; i++) {
-				let entry = possiblePath[i];
-				if (fs.existsSync(entry)) {
-					const resolved = path.relative(root, entry.replace(/\.ts$/, '.js'));
+    if (possiblePath.length) {
+      for (let i = 0; i < possiblePath.length; i++) {
+        const entry = possiblePath[i];
+        if (fs.existsSync(entry)) {
+          const resolved = path.relative(root, entry.replace(/\.ts$/, '.js'));
 
-					if (!resolved.startsWith('.')) {
-						return './' + resolved;
-					}
+          if (!resolved.startsWith('.')) {
+            return './' + resolved;
+          }
 
-					return resolved;
-				}
-			}
-		}
-	}
+          return resolved;
+        }
+      }
+    }
+  }
 
-	return null;
+  return null;
 }
 
 function replace(filePath, outFilePath, options) {
-	const code    = fs.readFileSync(filePath).toString();
-	const newCode = code.replace(
-		/(import|export) (.+?) from ('[^\n']+'|"[^\n"]+");/gs,
-		function (found, action, imported, from) {
-			const importPath   = from.slice(1, -1);
-			const resolvedPath = resolveImportPath(filePath, importPath, options);
+  const code = fs.readFileSync(filePath).toString();
+  const newCode = code.replace(
+    /(import|export) (.+?) from ('[^\n']+'|"[^\n"]+");/gs,
+    (found, action, imported, from) => {
+      const importPath = from.slice(1, -1);
+      const resolvedPath = resolveImportPath(filePath, importPath, options);
 
-			if (resolvedPath) {
-				console.log('\t', importPath, resolvedPath);
-				return `${action} ${imported} from '${resolvedPath.replaceAll('\\','/')}';`;
-			}
+      if (resolvedPath) {
+        console.log('\t', importPath, resolvedPath);
+        return `${action} ${imported} from '${resolvedPath.replaceAll(
+          '\\',
+          '/'
+        )}';`;
+      }
 
-			return found;
-		});
+      return found;
+    }
+  );
 
-	if (code !== newCode) {
-		fs.writeFileSync(outFilePath, newCode);
-	}
+  if (code !== newCode) {
+    fs.writeFileSync(outFilePath, newCode);
+  }
 }
 
 // Then, use it with a simple async for loop
 async function run(srcDir, options = defaultOptions) {
-	const {
-			  sourceFileFilter = defaultSourceFileFilter,
-		  } = options;
+  const {sourceFileFilter = defaultSourceFileFilter} = options;
 
-	for await (const entry of walk(srcDir)) {
-		if (sourceFileFilter(entry)) {
-			console.log(entry);
-			replace(entry, entry, options);
-		}
-	}
+  for await (const entry of walk(srcDir)) {
+    if (sourceFileFilter(entry)) {
+      console.log(entry);
+      replace(entry, entry, options);
+    }
+  }
 }
 
 const defaultSourceFileFilter = function (sourceFilePath) {
-	return /\.(js|ts)$/.test(sourceFilePath) && !/node_modules/.test(sourceFilePath);
+  return (
+    /\.(js|ts)$/.test(sourceFilePath) && !/node_modules/.test(sourceFilePath)
+  );
 };
 
 const defaultModuleFilter = function (importedModule) {
-	return !path.isAbsolute(importedModule) && !importedModule.startsWith('@') && !importedModule.endsWith('.js');
+  return (
+    !path.isAbsolute(importedModule) &&
+    !importedModule.startsWith('@') &&
+    !importedModule.endsWith('.js')
+  );
 };
 
-const defaultOptions      = {
-	sourceFileFilter: defaultSourceFileFilter,
-	moduleFilter    : defaultModuleFilter,
+const defaultOptions = {
+  sourceFileFilter: defaultSourceFileFilter,
+  moduleFilter: defaultModuleFilter,
 };
 
-
-await run('./dist/esm', defaultOptions);
+run('./dist/esm', defaultOptions);
